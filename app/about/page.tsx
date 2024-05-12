24da0472acf457d10268fd445672173ad066f706
PolCarva <pablocarvalhogimenez@gmail.com>
2024-05-12 02:01:49 -0300

    Add Favicon

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='px-5 lg:px-16'>page</div>
+  )
+}
+
+export default page
---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className='px-16'>page</section>
+  )
+}
+
+export default page
