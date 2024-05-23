fc6e32ffcfb3f2db2edbd04fb06b454d79edf7c5
PolCarva <pablocarvalhogimenez@gmail.com>
2024-05-23 10:51:45 -0300

    chore: Update remote image patterns and API URLs

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "**",
+        protocol: "https",
+        hostname: "misodesign.up.railway.app",
+        port: "",
+        pathname: "/media/**",
       },
     ],
   },
--- stables/index.ts
+++ stables/index.ts
@@ -1,6 +1,4 @@
-const isDev = process.env.NODE_ENV === 'development';
-
 export const STABLES = {
-    API_URL: isDev ? 'http://localhost:3000/api' : "https://augusto.up.railway.app/api",
-    UPLOADS_URL: isDev ? 'http://localhost:3000/media' : "https://augusto.up.railway.app/media",
+    API_URL: "https://misodesign.up.railway.app/api",
+    UPLOADS_URL: "https://misodesign.up.railway.app/media",
 }
