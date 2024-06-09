9281a7640696a25cb2c6a8d5ef7cbaaf429e2cb4
PolCarva <pablocarvalhogimenez@gmail.com>
2024-06-09 15:09:35 -0300

    chore: Update about page to improve performance and fix image failing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,27 +6,32 @@ import React from "react";
 export const revalidate = 60;
 
 const page = async () => {
-  const data = await fetch(`${STABLES.API_URL}/config/66492c2c993d98b2271c1f45?locale=undefined&draft=false&depth=1`);
+  const data = await fetch(
+    `${STABLES.API_URL}/config/66492c2c993d98b2271c1f45?locale=undefined&draft=false&depth=1`
+  );
   const info = await data.json();
 
   //revalidatePath("/about")
-  
+
   return (
     <div className="px-5 lg:px-16 flex gap-5">
       <div className="w-10/12 flex flex-col md:flex-row mx-auto mt-16">
         <div className="w-full md:w-2/3 h-fit text-sm md:pr-16 pb-10 text-balance flex flex-col gap-5">
-          <p className="whitespace-pre-line">
-            {info?.bio}
-          </p>
+          <p className="whitespace-pre-line">{info?.bio}</p>
         </div>
         <div className="w-full md:w-1/3 mb-10 lg:w-1/4 mx-auto h-full -order-1 md:order-1">
-          <Image
-          className="w-full md:aspect-[9/16] lg:aspect-auto object-cover"
-            alt="Augusto Quincke"
-            height={info?.profilePicture?.height}
-            width={info?.profilePicture?.width}
-            src={`${STABLES.UPLOADS_URL}/${info?.profilePicture?.filename}`}
-          />
+          {info?.profilePicture &&
+            info?.profilePicture?.width &&
+            info?.profilePicture?.height &&
+            info?.profilePicture?.filename && (
+              <Image
+                className="w-full md:aspect-[9/16] lg:aspect-auto object-cover"
+                alt="Augusto Quincke"
+                height={info?.profilePicture?.height}
+                width={info?.profilePicture?.width}
+                src={`${STABLES.UPLOADS_URL}/${info?.profilePicture?.filename}`}
+              />
+            )}
         </div>
       </div>
     </div>
